883d6b57ce8564e05553beb4a727d939271a7012
nilanjan-cse <nilanjan4rocks@gmail.com>
2021-11-05 20:56:57 +0530

    commit

---
+++ .next/static/_rckzDRHyRhIoPUjxVa7e/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/807c80d30f549ff9.css","static/chunks/pages/index-b892b3022bc3defa.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
